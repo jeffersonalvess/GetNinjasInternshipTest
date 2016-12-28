255f94257540c3938354a4ed38d404b4243b195d
jeffersonalvess <asilva.jefferson@gmail.com>
2016-12-28 21:12:26 -0200

    Tabbed layout done with icons

---
+++ app/src/main/java/com/getninjas/test/jeffersonalvess/getninjastest/LeadsFragment.java
@@ -0,0 +1,29 @@
+package com.getninjas.test.jeffersonalvess.getninjastest;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LeadsFragment extends Fragment {
+
+    public LeadsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_leads, container, false);
+    }
+}
