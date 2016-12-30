25c9b2ce1a2c3cd6ee6f6ca80710d5c614c6a03b
jeffersonalvess <asilva.jefferson@gmail.com>
2016-12-30 00:36:31 -0200

    Implementing new activity to show details about an offer/lead.
    TODO on this activity:
    1. Implement a list to show details;
    2. Include the buttons fixed on screen's bottom;
    2. Make it accessible from Leads;
    3. Make it dynamic for Offers and Leads;
    4. Implement Google Maps
    
    TODO on the Project:
    >> Improve REST implementation (I'm hardcoding the URL)
    >> Send intent to open phone and WhatsApp
    >> Pull to refresh (https://guides.codepath.com/android/using-the-recyclerview)

---
+++ app/src/main/java/com/getninjas/test/jeffersonalvess/getninjastest/DetailsActivity.java
@@ -0,0 +1,26 @@
+package com.getninjas.test.jeffersonalvess.getninjastest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //  TODO: Change window title based on who called the activity.
+        getSupportActionBar().setTitle("Offer");
+
+
+    }
+
+}
